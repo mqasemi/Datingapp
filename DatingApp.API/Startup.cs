2954a94fcc012117698c643bc7fb78f38a983c1d
ms-ghasemi <ms-ghasemi@agri-bank.com>
2020-01-26 09:02:20 +0330

    add corss support

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -29,6 +29,7 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>(X =>X.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,7 +45,7 @@ namespace DatingApp.API
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseCors(X=>X.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
--- Datingapp-SPA/src/app/app.module.ts
+++ Datingapp-SPA/src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ValuesComponent } from './values/values.component';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ValuesComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
